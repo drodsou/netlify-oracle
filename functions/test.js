c056f9352c1ffebbd075f3c914a795fd6648ae92
drodsou <gitman@drodsou.com>
2021-03-26 19:06:37 +0000

    test function

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,7 @@
 exports.handler = async function (event, context) {
 	return {
 		statusCode : 200,
-		body : JSON.stringify({test: "all right"})
+		body : JSON.stringify({test: "all right", src: __filename})
 	}
 	
 }
