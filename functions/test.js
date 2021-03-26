2daf84cf4e68f8ec1b40d6e5a5e1b273157d366a
drodsou <gitman@drodsou.com>
2021-03-26 19:27:14 +0000

    ...

--- functions/test.js
+++ functions/test.js
@@ -3,7 +3,15 @@ const fs = require('fs');
 exports.handler = async function (event, context) {
 	return {
 		statusCode : 200,
-		body : JSON.stringify({test: "all right", src: __filename, dir: fs.readdirSync(__dirname + '/..') })
+		body : JSON.stringify({
+			test: "all right", 
+			src: __filename, 
+			dirs: [
+				fs.readdirSync(__dirname + '/..'),
+				fs.readdirSync(__dirname + '/../..'),
+				fs.readdirSync(__dirname + '/../../..'),
+			]
+		},null,2)
 	}
 	
 }
