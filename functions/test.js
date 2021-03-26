c227c297853126dfbde01ab7793dc027c812a85e
drodsou <gitman@drodsou.com>
2021-03-26 19:21:48 +0000

    looking for prod abs path with test func

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
+
 exports.handler = async function (event, context) {
 	return {
 		statusCode : 200,
-		body : JSON.stringify({test: "all right", src: __filename})
+		body : JSON.stringify({test: "all right", src: __filename, dir: fs.readdirSync(__dirname + '/..') })
 	}
 	
 }
