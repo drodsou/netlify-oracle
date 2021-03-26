d77a5ce6933bc1234917ba19f234757c60335d50
drodsou <gitman@drodsou.com>
2021-03-26 19:53:22 +0000

    ...

--- functions/test.js
+++ functions/test.js
@@ -6,6 +6,7 @@ exports.handler = async function (event, context) {
 		body : JSON.stringify({
 			test: "all right", 
 			src: __filename, 
+			node_modules: require.resolve('oracledb'),
 			dirs: fs.readdirSync(event.queryStringParameters.dir||__dirname),
 			
 		},null,2)
